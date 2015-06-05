551c2cddc768f40f1282c04005a7dbe5964cec86
Robert Herhold <reherhold@gmail.com>
2015-06-05 15:41:51 -0400

    Fix errors from last commit

--- src/main/java/com/blocklaunch/blwarps/Constants.java
+++ src/main/java/com/blocklaunch/blwarps/Constants.java
@@ -19,11 +19,13 @@ public class Constants {
     public static final Text WARP_NOT_FOUND_MSG = Texts.of(TextColors.RED, PREFIX + " A warp with that name could not be found!");
     public static final Text GROUP_NOT_FOUND_MSG = Texts.of(TextColors.RED, Constants.PREFIX, " That group with that name could not be found!");
 
+    public static final String WARP_SUCCESS_MSG = PREFIX + " You have been warped to ";
     public static final String ERROR_WARPING_MSG = PREFIX + " There was an error scheduling your warp: ";
     public static final Text NO_WARPS_MSG = Texts.of(TextColors.GREEN, PREFIX + " There were no warps to display.");
     public static final Text MUST_BE_PLAYER_MSG = Texts.of(TextColors.RED, PREFIX + " You must be a player to send that command (not console)");
     public static final Text NO_PERMISSION_MSG = Texts.of(TextColors.RED, PREFIX + " You do not have permission to use that warp!");
     public static final Text INVENTORY_FULL_MSG = Texts.of(TextColors.RED, PREFIX + " Your inventory is full! Please clear a slot and try again.");
     public static final Text NO_WARPS_AFFECTED_MSG = Texts.of(TextColors.RED, PREFIX + " No warps were affected!");
+    public static final Text WORLD_NOT_FOUND_MSG = Texts.of(TextColors.RED, PREFIX + " The world you requested to be warped to could not be found!");
 
 }
--- src/main/java/com/blocklaunch/blwarps/runnables/WarpPlayerRunnable.java
+++ src/main/java/com/blocklaunch/blwarps/runnables/WarpPlayerRunnable.java
@@ -1,11 +1,11 @@
 package com.blocklaunch.blwarps.runnables;
 
 import org.spongepowered.api.entity.player.Player;
-import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.Texts;
 import org.spongepowered.api.text.format.TextColors;
 
 import com.blocklaunch.blwarps.BLWarps;
+import com.blocklaunch.blwarps.Constants;
 import com.blocklaunch.blwarps.Warp;
 
 public class WarpPlayerRunnable implements Runnable {
@@ -13,10 +13,6 @@ public class WarpPlayerRunnable implements Runnable {
     private BLWarps plugin;
     private Player player;
     private Warp warp;
-
-    private static final String WARP_SUCCESS_MSG = BLWarps.PREFIX + " You have been warped to ";
-    private static final Text WORLD_NOT_FOUND_MSG = Texts.of(TextColors.RED, BLWarps.PREFIX
-            + " The world you requested to be warped to could not be found!");
 
     public WarpPlayerRunnable(BLWarps plugin, Player player, Warp warp) {
         this.plugin = plugin;
@@ -27,9 +23,9 @@ public class WarpPlayerRunnable implements Runnable {
     @Override
     public void run() {
         if (!player.transferToWorld(warp.getWorld(), warp.getPosition())) {
-            player.sendMessage(WORLD_NOT_FOUND_MSG);
+            player.sendMessage(Constants.WORLD_NOT_FOUND_MSG);
         } else {
-            player.sendMessage(Texts.builder(WARP_SUCCESS_MSG).color(TextColors.GREEN).append(plugin.getUtil().generateWarpText(warp))
+            player.sendMessage(Texts.builder(Constants.WARP_SUCCESS_MSG).color(TextColors.GREEN).append(plugin.getUtil().generateWarpText(warp))
                     .build());
 
         }
