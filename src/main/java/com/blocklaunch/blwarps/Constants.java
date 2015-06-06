df42553ba2cd97e6ed224770f5dad8be7e2c7950
Robert Herhold <reherhold@gmail.com>
2015-06-06 10:53:21 -0400

    More work on create region command

--- src/main/java/com/blocklaunch/blwarps/Constants.java
+++ src/main/java/com/blocklaunch/blwarps/Constants.java
@@ -7,6 +7,7 @@ import org.spongepowered.api.text.format.TextColors;
 public class Constants {
 
     public static final String PREFIX = "[BLWarps]";
+    public static final int TICKS_PER_SECOND = 20;
 
     public static final Text SUCCESS_CREATE_WARP_MSG = Texts.of(TextColors.GREEN, PREFIX + " You have successfully created a warp: ");
     public static final String ERROR_CREATE_WARP_MSG = PREFIX + " There was an error creating the warp: ";
@@ -15,10 +16,20 @@ public class Constants {
     public static final Text SPECIFY_WARP_MSG = Texts.of(TextColors.RED, PREFIX + " You must specify a warp!");
     public static final Text SPECIFY_GROUP_MSG = Texts.of(TextColors.RED, PREFIX + " You must specify a group!");
     public static final Text SPECIFY_REGION_MSG = Texts.of(TextColors.RED, PREFIX, " You must specify a region!");
+    public static final Text SPECIFY_2_CORNERS_MSG = Texts.of(TextColors.RED, PREFIX, " You must specify two locations!");
 
     public static final Text WARP_NOT_FOUND_MSG = Texts.of(TextColors.RED, PREFIX + " A warp with that name could not be found!");
     public static final Text GROUP_NOT_FOUND_MSG = Texts.of(TextColors.RED, Constants.PREFIX, " That group with that name could not be found!");
+    
+    public static final String WARP_NAME_EXISTS = "A warp with that name already exists!";
+    public static final String WARP_LOCATION_EXISTS = "A warp at that location already exists!";
+    public static final String WARP_REGION_NAME_EXISTS = "A warp region with that name already exists!";
+    public static final String WARP_REGION_LOCATION_EXISTS = "A warp region in that area already exists!";
+    
+    public static final String ERROR_FILE_WRITE = "There was an error writing to the file!";
+    public static final String ERROR_FILE_READ = "There was an error reading the warps file!";
 
+    public static final String ERROR_SCHEDULING_WARP = "There was an error scheduling your warp. Please try again.";
     public static final String WARP_SUCCESS_MSG = PREFIX + " You have been warped to ";
     public static final String ERROR_WARPING_MSG = PREFIX + " There was an error scheduling your warp: ";
     public static final Text NO_WARPS_MSG = Texts.of(TextColors.GREEN, PREFIX + " There were no warps to display.");
--- src/main/java/com/blocklaunch/blwarps/commands/executors/region/CreateWarpRegionExecutor.java
+++ src/main/java/com/blocklaunch/blwarps/commands/executors/region/CreateWarpRegionExecutor.java
@@ -1,5 +1,6 @@
 package com.blocklaunch.blwarps.commands.executors.region;
 
+import org.spongepowered.api.entity.player.Player;
 import org.spongepowered.api.util.command.CommandException;
 import org.spongepowered.api.util.command.CommandResult;
 import org.spongepowered.api.util.command.CommandSource;
@@ -8,23 +9,39 @@ import org.spongepowered.api.util.command.spec.CommandExecutor;
 
 import com.blocklaunch.blwarps.Constants;
 import com.blocklaunch.blwarps.Warp;
+import com.blocklaunch.blwarps.region.WarpRegion;
 import com.flowpowered.math.vector.Vector3d;
 import com.google.common.base.Optional;
 
 public class CreateWarpRegionExecutor implements CommandExecutor {
     @Override
     public CommandResult execute(CommandSource source, CommandContext args) throws CommandException {
+        if (!(source instanceof Player)) {
+            source.sendMessage(Constants.MUST_BE_PLAYER_MSG);
+            return CommandResult.empty();
+        }
+        // Make sure the source is a player before attempting to get their
+        // world
+        Player player = (Player) source;
+
         Optional<String> regionNameOpt = args.getOne("name");
         Optional<Warp> linkedWarpOpt = args.getOne("warp");
         Optional<Vector3d> corner1Opt = args.getOne("corner1");
         Optional<Vector3d> corner2Opt = args.getOne("corner2");
-        
-        if(!regionNameOpt.isPresent()) {
+
+        if (!regionNameOpt.isPresent()) {
             source.sendMessage(Constants.SPECIFY_REGION_MSG);
             return CommandResult.empty();
         }
-        
+        if (!linkedWarpOpt.isPresent()) {
+            source.sendMessage(Constants.SPECIFY_WARP_MSG);
+        }
+        if (!corner1Opt.isPresent() || !corner2Opt.isPresent()) {
+            source.sendMessage(Constants.SPECIFY_2_CORNERS_MSG);
+        }
+
+        WarpRegion region = new WarpRegion(linkedWarpOpt.get(), regionNameOpt.get(), player.getWorld().getName(), corner1Opt.get(), corner2Opt.get());
+
         return null;
     }
-
 }
